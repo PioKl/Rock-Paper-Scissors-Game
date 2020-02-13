9547aef29e4d63fa6e8fa0f283b069614f2889d1
PioKl <kosiu342@gmail.com>
2020-02-13 18:52:36 +0100

    Updates

---
+++ precache-manifest.bb6b2a3906351663bf12feff6ddaa8c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af30919156a57939d464",
+    "url": "/Rock-Paper-Scissors-Game/static/css/main.f688c9b5.chunk.css"
+  },
+  {
+    "revision": "af30919156a57939d464",
+    "url": "/Rock-Paper-Scissors-Game/static/js/main.3242131e.chunk.js"
+  },
+  {
+    "revision": "a700dd80aa6135fadc8f",
+    "url": "/Rock-Paper-Scissors-Game/static/js/runtime~main.6497de0f.js"
+  },
+  {
+    "revision": "5502f4c353b899b2d6c9",
+    "url": "/Rock-Paper-Scissors-Game/static/js/2.bc8a9427.chunk.js"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/Rock-Paper-Scissors-Game/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/Rock-Paper-Scissors-Game/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/Rock-Paper-Scissors-Game/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/Rock-Paper-Scissors-Game/static/media/logo.759a2075.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/Rock-Paper-Scissors-Game/static/media/image-rules.5d74d25d.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/Rock-Paper-Scissors-Game/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/Rock-Paper-Scissors-Game/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "2f164a525bb703684d4572178152e5d3",
+    "url": "/Rock-Paper-Scissors-Game/index.html"
+  }
+];

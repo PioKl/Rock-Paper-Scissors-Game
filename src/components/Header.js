e7cbce3edf13cbb6f2eb9ce2cc8402fd28eb6b2e
PioKl <kosiu342@gmail.com>
2020-02-28 22:27:27 +0100

    Utworzenie nowego komp.funkcyjnego i styli Header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style/Header.scss';
+import logo from '../images/logo.svg';
+import logoExtended from '../images/logo-bonus.svg';
+
+const Header = (props) => {
+    return (
+        <div className="header">
+            <div className='title'>
+                <a href="mainMenu"><img className="logoImg" src={props.extendedVersion ? logoExtended : logo} alt={logo} /></a>
+            </div>
+            <div className='pointsContainer'>
+                <p className='points-title'>SCORE</p>
+                <p className='points-count'>{props.points}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;

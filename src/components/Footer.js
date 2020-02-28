33479b246d2fb0ba82cb17b96ce9f55d25879e7c
PioKl <kosiu342@gmail.com>
2020-02-28 22:39:15 +0100

    Utworzenie nowego komp.funkcyjnego i styli Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" className="footerLink">Frontend Mentor</a>.
+            Coded by <a href="https://github.com/PioKl" target="_blank" rel="noopener noreferrer" className="footerLink">Piotr Kłosowski</a>.
+        </div>
+    );
+}
+
+export default Footer;
